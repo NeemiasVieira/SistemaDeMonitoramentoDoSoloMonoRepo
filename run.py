e52e0b2897c8a54186e66dbce1d3ba4a1cb1c133
Neemias Vieira <neemias.vieira@live.com>
2024-10-17 17:40:41 -0300

    fix: allow cors

--- run.py
+++ run.py
@@ -1,5 +1,14 @@
 from app import app
+from flask import Flask, request, jsonify
 #executar export PYTHONDONTWRITEBYTECODE=1
+
+@app.after_request
+def add_cors_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type,Authorization"
+    response.headers["Access-Control-Allow-Methods"] = "GET,POST,PUT,DELETE,OPTIONS"
+    return response
+
 if __name__ == '__main__':
     # Iniciando o aplicativo Flask
     app.run(debug=True, port=8080, host='0.0.0.0')
